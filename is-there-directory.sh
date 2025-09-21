05db6307f609a2d314fb1f1bade1a507088f4dcc
matthews <cnusrinivasu@gmail.com>
2025-09-21 18:49:59 +0530

    is there directory or not

--- is-there-directory.sh
+++ is-there-directory.sh
@@ -4,8 +4,8 @@ SOURCE_DIR=/home/ec2-user/expense-source
 
 if [ -d $SOURCE_DIR ]
 then 
-    echo "The $SOURCE_DIR exists..."
+    echo "The source directory exists..."
 else
-    echo "The $SOURCE_DIR does not exit..."
+    echo "The source directory does not exit..."
     exit 1
 fi
