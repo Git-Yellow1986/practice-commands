c26336a217fe5873f5526915fa1bca68dccfe10b
matthews <cnusrinivasu@gmail.com>
2025-09-21 23:24:46 +0530

    is there directory or not

--- log-file.sh
+++ log-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOG_FOLDER="/var/log/shell-script"
-TIME_STAMP=$(data %Y-%m-%d-%H-%M-%S)
+TIME_STAMP=$(date %Y-%m-%d-%H-%M-%S)
 FILE_NAME=$(touch redictors)
 LOG_FILE="$LOG_FOLDER/$FILE_NAME-$TIME_STAMP.log"
 mkdir -p $LOG_FOLDER
