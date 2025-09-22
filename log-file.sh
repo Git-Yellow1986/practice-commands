e2d28c205b308d76f4764bd3b77eb3a04889d859
matthews <cnusrinivasu@gmail.com>
2025-09-22 10:04:06 +0530

    is there directory or not

--- log-file.sh
+++ log-file.sh
@@ -9,7 +9,7 @@ mkdir -p $LOG_FOLDER
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
-    echo "Please run this script root privelage" &>>$LOG_FILE
+    echo "Please run this script root privelage $1" &>>$LOG_FILE
     exit -1
 fi
 
