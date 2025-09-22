cac00de6eeae27b4a36ee19e35eb5fce971d9e7d
matthews <cnusrinivasu@gmail.com>
2025-09-22 09:37:07 +0530

    is there directory or not

--- log-file.sh
+++ log-file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LOG_FOLDER="/var/log/shell-script/"
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
-FILE_NAME=$(touch matthews.log | cut "." -f1)
+FILE_NAME=$(echo matthews.log | cut "." -f1)
 LOG_FILE="$LOG_FOLDER/$FILE_NAME-$TIME_STAMP.log"
 mkdir -p $LOG_FOLDER
 
