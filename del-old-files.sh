bbe326744a0cecf6290b85c3351e3d5921e54a02
matthews <cnusrinivasu@gmail.com>
2025-09-22 18:36:42 +0530

    old files

--- del-old-files.sh
+++ del-old-files.sh
@@ -5,6 +5,8 @@ SOURCE_DIR="/var/log/shell-script"
 if [ ! -d $SOURCE_DIR ]
 then
     echo "The Source file does not exit...."
+    exit 1
 else
     echo "The file is Exist...."
+    ls -l $SOURCE_DIR
 fi
