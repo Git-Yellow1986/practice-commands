417974c820c042c0663a7bcd59dfb6b5204a492b
matthews <cnusrinivasu@gmail.com>
2025-09-22 11:37:19 +0530

    is there directory or not

--- log-file.sh
+++ log-file.sh
@@ -16,14 +16,24 @@ Y="\e[32"
 echo "Script started executing at: $(date)"
 
 
-VALIDATE(){
-if [ $1 -ne 0 ]
-then 
-    echo -e "$2 is ... $R FAILURE $N"
-    exit 1
-else 
-    echo -e "$2 is... $G success $N"
-fi
+# VALIDATE(){
+# if [ $1 -ne 0 ]
+# then 
+#     echo -e "$2 is ... $R FAILURE $N"
+#     exit 1
+# else 
+#     echo -e "$2 is... $G success $N"
+# fi
+# }
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is ...$R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is ...$G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
 if [ $USERID -ne 0 ]
