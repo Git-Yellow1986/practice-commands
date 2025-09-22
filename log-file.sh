07014a1d5bdefa5777fb283831a2fb44d08a8977
matthews <cnusrinivasu@gmail.com>
2025-09-22 11:55:15 +0530

    is there directory or not

--- log-file.sh
+++ log-file.sh
@@ -38,7 +38,7 @@ dnf install nginxx -y &>>$LOG_FILE
 
 if [ $1 -ne 0 ]
 then 
-    echo -e "$2 Nginx install... $R FAILURE $N"
+    echo -e "$1 Nginx install... $R FAILURE $N"
     exit 1
 else 
     echo -e "$2 Nginx insall.. $Y SUCCESSFULLY $N"
