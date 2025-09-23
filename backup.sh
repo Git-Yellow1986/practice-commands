f23a6caaf065204f8c1b5cc4c1dd9fc617e29f84
matthews <cnusrinivasu@gmail.com>
2025-09-23 21:42:43 +0530

    backup 123

--- backup.sh
+++ backup.sh
@@ -41,12 +41,12 @@ fi
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
     echo "Files: $FILE"
     
-    if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
-    then
-        echo "Files are found"
-        ZIP_FILE="$DEST_DIR/app-log-$TIMESTAMP.zip"
-        find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
-    fi
+    # if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
+    # then
+    #     echo "Files are found"
+    #     ZIP_FILE="$DEST_DIR/app-log-$TIMESTAMP.zip"
+    #     find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
+    # fi
 
 
 
